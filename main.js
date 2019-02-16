d8a751fc7bddd92846275cc3d638056d64dedd49
kberaki <berakik@gmail.com>
2019-02-15 20:49:38 -0500

    js file updated

---
+++ main.js
@@ -0,0 +1,17 @@
+//let name = kesete;
+//let age = 20;
+
+//if(age=>20){
+ //alert( "  You are an adult!")
+//} else{
+ // alert("  you need to grow up!")
+//}
+
+let speedLimit =35;
+let currentSpeed = 45;
+
+if(currentSpeed>speedLimit){
+  alert("Take a picture")
+}else{
+  alert("Keep it up")
+}
